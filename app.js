dfe02dc6dabfad67fed6d4fb09eb3125e2b3c29d
Alessandro Chumpitaz <par_214@hotmail.com>
2021-09-30 19:35:25 -0500

    Mini fix for deployment

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app)
-const io = require('socket.io')(server, {
+const index = '/index.html'
+const port = process.env.PORT || 3000;
+
+const express = require('express')
+const app = express()
+  .use((req, res) => res.sendFile(index, { root: __dirname}))
+  .listen(port, () => console.log(`Server running port ${port}`))
+
+const io = require('socket.io')(app, {
   options: {
     cors: '*'
   }
 });
 
-const port = process.env.PORT || 3000;
-
 io.on('connection', (socket) => {
   socket.on('join', (data) => {
-    const roomName = data.roomName;
+    const { idPeer, roomName } = data;
+    io.emit('join', `${idPeer} entered ${roomName}`)
     socket.join(roomName);
     socket.to(roomName).broadcast.emit('new-user', data)
 
     socket.on('disconnect', () => {
+      io.emit('disconnect', `${idPeer} exited ${roomName}`)
       socket.to(roomName).broadcast.emit('bye-user', data)
     })
   })
 })
 
-server.listen(port, () => {
-  console.log(`Server running port ${port}`)
-});
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
